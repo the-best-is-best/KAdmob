28ddd5f4b42ca62aa280b5eaca8df4385783918b
Michelle Raouf <michelle.raouf@52ndsolution.net>
2024-10-16 00:01:17 +0300

    make KRewardAdItem ver @objc and public for kmm

--- KAdmob/Classes/KRewaredItem.swift
+++ KAdmob/Classes/KRewaredItem.swift
@@ -2,10 +2,10 @@ import GoogleMobileAds
 import UIKit
 
 @objc public class KRewardAdItem: NSObject {
-    var type: String
-    var amount: Int
+    @objc public var type: String
+    @objc public var amount: Int
     
-    @objc public   init(_ type: String, _ amount: Int) {
+    @objc public init(_ type: String, _ amount: Int) {
         self.type = type
         self.amount = amount
     }
