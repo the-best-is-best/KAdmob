66e5adc21fbcd33a8a42902df8f15bb6e92bf053
Michelle Raouf <michelle.raouf@52ndsolution.net>
2024-10-15 16:57:49 +0300

    fix banner ad

--- KAdmob/Classes/BannerAdView.swift
+++ KAdmob/Classes/BannerAdView.swift
@@ -1,35 +1,25 @@
 import GoogleMobileAds
 import UIKit
 
-// Enum to define banner ad types
- enum KAdmobBannerType {
-    case banner
-    case fullBanner
-    case largeBanner
-}
-
 @objc public class KBannerAdView: UIView {
 
     private var bannerView: GAMBannerView!
 
-    // Enum to define banner ad types
     @objc public enum KAdmobBannerType: Int {
-        case banner
-        case fullBanner
-        case largeBanner
+        case banner = 0
+        case fullBanner = 1
+        case largeBanner = 2
     }
 
-    // Property to hold the ad unit ID, default is nil
-    @objc public var adUnitID: String? // Default is nil
+    @objc public var adUnitID: String? // Property to hold the ad unit ID, default is nil
 
-    // Initializer
     // Initializer that takes adUnitID and adType
     @objc public init(adUnitID: String?, adType: KAdmobBannerType) {
-         super.init(frame: .zero)
-         self.adUnitID = adUnitID
-         setupBannerView()
-         loadAd(ofType: adType)
-     }
+        super.init(frame: .zero)
+        self.adUnitID = adUnitID
+        setupBannerView()
+        loadAd(ofType: adType)
+    }
 
     required init?(coder: NSCoder) {
         super.init(coder: coder)
@@ -51,7 +41,6 @@ import UIKit
 
     // Public method to load an ad of a specific type
     @objc public func loadAd(ofType type: KAdmobBannerType) {
-        // Throw an exception if adUnitID is nil
         guard let adUnitID = adUnitID else {
             fatalError("Ad Unit ID cannot be nil. Please set a valid ad unit ID.")
         }
@@ -68,8 +57,6 @@ import UIKit
 
         // Update banner view size
         bannerView.adSize = adSize
-
-        // Set the ad unit ID and load an ad
         bannerView.adUnitID = adUnitID // Use the variable instead of a hardcoded string
         bannerView.load(GADRequest())
     }
